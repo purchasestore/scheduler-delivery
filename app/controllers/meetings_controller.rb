cc905c0779baeb503d0a42027b2f785393a782e4
puppe1990 <matheus.puppe90@hotmail.com>
2020-05-06 21:14:33 -0300

    Add strong params in meetings

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -75,6 +75,6 @@ class MeetingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def meeting_params
-      params.require(:meeting).permit(:name, :localization, :phone, :start_time)
+      params.require(:meeting).permit(:name, :localization, :phone, :start_time, :products, :total_price, :payment_type)
     end
 end
